7c06f7250ff1729cecf649cc5d01130ae59dad9c
Rcasas14 <reymartcasas123@gmail.com>
2017-10-27 00:48:05 +0800

    added minor changes, added log in form

--- index.php
+++ index.php
@@ -6,11 +6,10 @@
     <meta name="viewport" content="width=device-width, initial-scale-1">
     <link rel="shortcut icon" href="Images/icon.ico">
     <title>Grey Hotel</title>
-    <link href="UIKIT/css/uikit.min.css" rel="stylesheet">
     <link href="UIKIT/css/uikit.css" rel="stylesheet">
-    <link href="UIKIT/css/uikit-rtl.css" rel="stylesheet">
-    <link href="UIKIT/css/uikit-rtl.min.css" rel="stylesheet" type="text/css">
-    <link href="style.css" type="text/css" rel="stylesheet">
+    <link href="owl-carousel/css/owl.carousel.css">
+    <link href="owl-carousel/css/owl.theme.default.css">
+    <link href="style.css" rel="stylesheet" type="text/css">
 
 
 
@@ -23,7 +22,7 @@
                 <img src="Images/logo.png" alt="Grey Hotel" class="ico-image">
             </div>
             <div>
-                
+
                 <button type="button" class="login-but"><a href="login-page.php">LOGIN</a></button>
             </div>
             <div class="topnav" id="myTopnav">
@@ -33,35 +32,29 @@
                 <a href="#booking" class="h">BOOK NOW</a>
             </div>
         </div>
+<div class="owl-carousel owl-theme">
+    <div class="item"><h4>1</h4></div>
+    <div class="item"><h4>2</h4></div>
+    <div class="item"><h4>3</h4></div>
+    <div class="item"><h4>4</h4></div>
+</div>
 
-        <div class="uk-position-relative uk-visible-toggle uk-light" uk-slideshow>
-
-            <ul class="uk-slideshow-items">
-                <li>
-                    <img src="Images/Greatness.jpg" alt="" uk-cover>
-                </li>
-                <li>
-                    <img src="Images/Relax.jpeg" alt="" uk-cover>
-                </li>
-                <li>
-                    <img src="Images/Experience.jpeg" alt="" uk-cover>
-                </li>
-                <li>
-                    <img src="Images/Youth.jpeg" alt="" uk-cover>
-                </li>
-            </ul>
-
-            <a class="uk-position-center-left uk-position-small uk-hidden-hover" href="#" uk-slidenav-previous uk-slideshow-item="previous"></a>
-            <a class="uk-position-center-right uk-position-small uk-hidden-hover" href="#" uk-slidenav-next uk-slideshow-item="next"></a>
-
-        </div>
+    </div>
 
 
     </div>
-    <script src="UIKIT/js/uikit.min.js"></script>
+
+    <script src="Jquery/jquery.min.js"></script>
     <script src="UIKIT/js/uikit.js"></script>
     <script src="UIKIT/js/uikit-icons.js"></script>
-    <script src="UIKIT/js/uikit-icons.min.js"></script>
+    <script src="owl-carousel/js/owl.carousel.js"></script>
+    <script>
+        $('.owl-carousel').owlCarousel({
+            loop: true,
+            margin: 10,
+            nav: true
+        });
+    </script>
 
 </body>
 
--- login-page.php
+++ login-page.php
@@ -19,19 +19,53 @@
             <div class="ico">
                 <img src="Images/logo.png" alt="Grey Hotel" class="ico-image">
             </div>
-            
+
             <div class="topnav" id="myTopnav">
                 <a href="index.php" class="h">HOME</a>
                 <a href="#room" class="h">ROOMS</a>
                 <a href="#contact" class="h">CONTACT US</a>
+                <a href="#booking" class="h">BOOK NOW</a>
             </div>
         </div>
-        <div class="log-container">
-            <div>
-
-            </div>
-
+     <!--   <div class="head-image">
+        <h1 class="head-wel">WELCOME BACK!</h1>
+            <img src="Images/welcome.jpeg" class="Headlog-image">
+        </div>-->
+        <div class="log-container uk-clearfix">
+            <!-- must have action_page.php-->
+            <form class="form-size uk-horizontal">
+                <fieldset class="uk-fieldset">
+                    <legend class="uk-legend" id="log-text">LOGIN AS OUR CLIENT</legend>
+                    <div class="uk-margin">
+                        <label class="uk-form-label form-horizontal-text">Email Address *</label>
+                        <div class="uk-form-controls">
+                            <input class="uk-input uk-form-small" id="form-horizontal-text" type="Email" placeholder="name@example.com">
+                            <br>
+                            <br>
+                            <label class="uk-form-label form-horizontal-text">Password*</label>
+                            <input class="uk-input uk-form-small" id="form-horizontal-text" type="password">
+                            <br>
+                            <br>
+                            <button class=" uk-button-default log-butt">LOGIN</button>
+                            <div class="uk-margin uk-grid-small uk-child-width-auto uk-grid">
+                                <label>
+                                    <input class="uk-checkbox" type="checkbox"> Login as administrator &nbsp;&nbsp;&nbsp;&nbsp;
+                                    <input class="uk-checkbox" type="checkbox"> Remember me</label>
+                            </div>
+                            <a href=#admin-page>Forgot your password?</a>
+                            <br>
+                            <br>
+                            <br>
+                        </div>
+                        <div class="not-member-container">
+                            <p class="notAmember-yet">NOT A MEMBER YET?</p>
+                            <p id="create-acc">Create an Account <span><a href="#register-page.php">here!</a></span></p>
+                        </div>
+                    </div>
         </div>
+        </fieldset>
+        </form>
+    </div>
     </div>
     <script src="UIKIT/js/uikit.min.js"></script>
     <script src="UIKIT/js/uikit.js"></script>
