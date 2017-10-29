23cbee4064d53875ff9203519d9b38b7ae9c9b22
EvaAparece <eva.d.aparece@gmail.com>
2017-10-29 11:26:48 +0800

    created a welcomeAdmin page

---
+++ welcomeAdmin_Page.php
@@ -0,0 +1,46 @@
+</!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale-1">
+    <link rel="shortcut icon" href="Images/icon.ico">
+    <title>Grey Hotel</title>
+    <link href="UIKIT/css/uikit.css" rel="stylesheet">
+    <link href="style.css" rel="stylesheet" type="text/css">
+</head>
+
+<body class="body">
+        <div class="top-bg-color">
+            <div class="ico">
+                <img src="Images/logo.png" alt="Grey Hotel" class="ico-image">
+            </div>
+            <div class="adminHeader">
+                <h1>Grey Hotel ADMIN</h1>
+            </div>
+        <div>
+        <aside class="sidebar">
+            <center><h4>Admin Menu</h4></center>
+            <div class="sidebar_links">
+                <ul>
+                    <li><a href="#manage website content" class="h">Manage Website Content</a></li>
+                    <br>
+                    <li><a href="#manage clients" class="h">Manage Clients</a></li>
+                    <br>
+                    <li><a href="#manage admin users" class="h">Manage Admin users</a></li>
+                    <br>
+                    <li><a href="#log out" class="h">Log out</a></li>
+                </ul>
+            </div>
+        </aside>
+
+        <div class="content">
+            <center><h4>CONTENT</h4></center>
+        </div>
+
+    <script src="Jquery/jquery.min.js"></script>
+    <script src="UIKIT/js/uikit.js"></script>
+    <script src="UIKIT/js/uikit-icons.js"></script>
+</body>
+
+</html>
