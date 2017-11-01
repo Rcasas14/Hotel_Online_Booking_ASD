469a372b4703f760a6977d90bf36ed1eb6075b85
EvaAparece <eva.d.aparece@gmail.com>
2017-11-01 17:54:35 +0800

    created a page for rooms/room types

---
+++ rooms.php
@@ -0,0 +1,117 @@
+</!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale-1">
+    <link rel="shortcut icon" href="Images/icon.ico">
+    <title>Grey Hotel</title>
+    <link href="UIKIT/css/uikit.min.css" rel="stylesheet">
+    <link href="UIKIT/css/uikit.css" rel="stylesheet">
+    <link href="UIKIT/css/uikit-rtl.css" rel="stylesheet">
+    <link href="UIKIT/css/uikit-rtl.min.css" rel="stylesheet" type="text/css">
+    <link href="style.css" type="text/css" rel="stylesheet">
+</head>
+
+<body>
+    <div>
+        <div class="top-bg-color">
+            <div class="ico">
+                <img src="Images/logo.png" alt="Grey Hotel" class="ico-image">
+            </div>
+            <div class="topnav" id="myTopnav">
+                <a href="index.php" class="h">HOME</a>
+                <a href="#room" class="h">ROOMS</a>
+                <a href="#reserve-now" class="h">RESERVE NOW</a>
+            </div>
+        </div>
+        <div class="roomType">
+            <h3>Type A</h3>
+                <center>
+                    <div class="imgRow">
+                        <div class="imgDescription">
+                            <img src="Images/typeA.jpg" alt="Grey Hotel">
+                            <p><b>Room Type: </b></p>
+                            <p><b>Room Rate: </b></p>
+                            <p><b>Room Number: </b></p>
+                            <p><b>Available: </b></p>
+                        </div>
+                        <div class="imgDescription">
+                            <img src="Images/typeA.jpg" alt="Grey Hotel">
+                            <p><b>Room Type: </b></p>
+                            <p><b>Room Rate: </b></p>
+                            <p><b>Room Number: </b></p>
+                            <p><b>Available: </b></p>
+                        </div>
+                        <div class="imgDescription">
+                            <img src="Images/typeA.jpg" alt="Grey Hotel">
+                            <p><b>Room Type: </b></p>
+                            <p><b>Room Rate: </b></p>
+                            <p><b>Room Number: </b></p>
+                            <p><b>Available: </b></p>
+                        </div>
+                        
+                    </div>
+                </center>
+            <h3>Type B</h3> 
+                <center>
+                        <div class="imgRow">
+                            <div class="imgDescription">
+                            <img src="Images/typeA.jpg" alt="Grey Hotel">
+                            <p><b>Room Type: </b></p>
+                            <p><b>Room Rate: </b></p>
+                            <p><b>Room Number: </b></p>
+                            <p><b>Available: </b></p>
+                        </div>
+                        <div class="imgDescription">
+                            <img src="Images/typeA.jpg" alt="Grey Hotel">
+                            <p><b>Room Type: </b></p>
+                            <p><b>Room Rate: </b></p>
+                            <p><b>Room Number: </b></p>
+                            <p><b>Available: </b></p>
+                        </div>
+                        <div class="imgDescription">
+                            <img src="Images/typeA.jpg" alt="Grey Hotel">
+                            <p><b>Room Type: </b></p>
+                            <p><b>Room Rate: </b></p>
+                            <p><b>Room Number: </b></p>
+                            <p><b>Available: </b></p>
+                        </div>
+                        </div>
+                    </center>
+            <h3>Type C</h3> 
+                <center>
+                    <div class="imgRow">
+                        <div class="imgDescription">
+                            <img src="Images/typeA.jpg" alt="Grey Hotel">
+                            <p><b>Room Type: </b></p>
+                            <p><b>Room Rate: </b></p>
+                            <p><b>Room Number: </b></p>
+                            <p><b>Available: </b></p>
+                        </div>
+                        <div class="imgDescription">
+                            <img src="Images/typeA.jpg" alt="Grey Hotel">
+                            <p><b>Room Type: </b></p>
+                            <p><b>Room Rate: </b></p>
+                            <p><b>Room Number: </b></p>
+                            <p><b>Available: </b></p>
+                        </div>
+                        <div class="imgDescription">
+                            <img src="Images/typeA.jpg" alt="Grey Hotel">
+                            <p><b>Room Type: </b></p>
+                            <p><b>Room Rate: </b></p>
+                            <p><b>Room Number: </b></p>
+                            <p><b>Available: </b></p>
+                        </div>
+                    </div>
+                </center>
+        </div>
+        
+    </div>
+    <script src="UIKIT/js/uikit.min.js"></script>
+    <script src="UIKIT/js/uikit.js"></script>
+    <script src="UIKIT/js/uikit-icons.js"></script>
+    <script src="UIKIT/js/uikit-icons.min.js"></script>
+</body>
+
+</html>
