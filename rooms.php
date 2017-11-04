5ac286833d3048485868cd5f8fd436ea5829ec20
EvaAparece <eva.d.aparece@gmail.com>
2017-11-04 12:40:21 +0800

    added images, changed the images in rooms.php

--- rooms.php
+++ rooms.php
@@ -10,102 +10,157 @@
     <link href="UIKIT/css/uikit.css" rel="stylesheet">
     <link href="UIKIT/css/uikit-rtl.css" rel="stylesheet">
     <link href="UIKIT/css/uikit-rtl.min.css" rel="stylesheet" type="text/css">
-    <link href="style.css" type="text/css" rel="stylesheet">
+    <link href="style2.css" type="text/css" rel="stylesheet">
+    <link href="../tes-css.css" rel="stylesheet" type="text/css">
+    <link href="footer-distributed.css" rel="stylesheet" type="text/css">
+    <link href="styles_Rooms.css" rel="stylesheet" type="text/css">
 </head>
 
 <body>
     <div>
         <div class="top-bg-color">
             <div class="ico">
-                <img src="Images/logo.png" alt="Grey Hotel" class="ico-image">
+                <a href="index.php"><img src="Images/logo.png" alt="Grey Hotel" class="ico-image"></a>
             </div>
             <div class="topnav" id="myTopnav">
-                <a href="index.php" class="h">HOME</a>
-                <a href="#room" class="h">ROOMS</a>
-                <a href="#reserve-now" class="h">RESERVE NOW</a>
+                <div class="dropdown">
+                    <p class="nav-text"><a href="index.php" class="link-head-text" uk-scroll>HOME</a></p>
+                    <div class="dropdown-content">
+                        <a href="#about-hotel" uk-scroll>About Grey Hotel</a>
+                        <a href="#contact">Contact Us</a>
+                    </div>
+                </div>
+                <p class="nav-text"><a href="#rooms" class="link-head-text">ROOMS</a></p>
+                <div class="dropdown">
+                    <p class="nav-text"><a href="login-page.php" class="link-head-text">RESERVE NOW</a></p>
+                    <div class="dropdown-content">
+                        <a href="reserve-form-page.php">Reserve Room</a>
+                        <a href="#client-profile">Client Profile</a>
+                    </div>
+                </div>
             </div>
         </div>
         <div class="roomType">
-            <h3>Type A</h3>
+            <h3>Rooms</h3>
                 <center>
                     <div class="imgRow">
                         <div class="imgDescription">
-                            <img src="Images/typeA.jpg" alt="Grey Hotel">
-                            <p><b>Room Type: </b></p>
-                            <p><b>Room Rate: </b></p>
+                            <img src="Images/mp_superiorRoom.jpg" alt="Grey Hotel">
+                            <p><b>Room Type:</b> Superior Room</p>
+                            <p><b>Room Rate: </b>starts at Php 3,600/night</p>
                             <p><b>Room Number: </b></p>
                             <p><b>Available: </b></p>
                         </div>
                         <div class="imgDescription">
-                            <img src="Images/typeA.jpg" alt="Grey Hotel">
-                            <p><b>Room Type: </b></p>
-                            <p><b>Room Rate: </b></p>
+                            <img src="Images/mp_deluxeRoom.jpg" alt="Grey Hotel">
+                            <p><b>Room Type:</b>Deluxe Room</p>
+                            <p><b>Room Rate: </b>starts at Php 3,800/night</p>
                             <p><b>Room Number: </b></p>
                             <p><b>Available: </b></p>
                         </div>
                         <div class="imgDescription">
-                            <img src="Images/typeA.jpg" alt="Grey Hotel">
-                            <p><b>Room Type: </b></p>
-                            <p><b>Room Rate: </b></p>
+                            <img src="Images/mp_premier.jpg" alt="Grey Hotel">
+                            <p><b>Room Type:</b> Premier Room</p>
+                            <p><b>Room Rate: </b>starts at Php 4,800/night</p>
                             <p><b>Room Number: </b></p>
                             <p><b>Available: </b></p>
                         </div>
                         
                     </div>
                 </center>
-            <h3>Type B</h3> 
+            <h3>Suites</h3> 
                 <center>
                         <div class="imgRow">
                             <div class="imgDescription">
-                            <img src="Images/typeA.jpg" alt="Grey Hotel">
-                            <p><b>Room Type: </b></p>
-                            <p><b>Room Rate: </b></p>
+                            <img src="Images/mp_deluxeSuite.jpg" alt="Grey Hotel">
+                            <p><b>Room Type: </b> Deluxe Suite</p>
+                            <p><b>Room Rate: </b>starts at Php 10,000/night</p>
                             <p><b>Room Number: </b></p>
                             <p><b>Available: </b></p>
                         </div>
                         <div class="imgDescription">
-                            <img src="Images/typeA.jpg" alt="Grey Hotel">
-                            <p><b>Room Type: </b></p>
-                            <p><b>Room Rate: </b></p>
+                            <img src="Images/mp_cabanaSuites.jpg" alt="Grey Hotel">
+                            <p><b>Room Type: </b>Cabana Suite</p>
+                            <p><b>Room Rate: </b>starts at Php 12,200/night</p>
                             <p><b>Room Number: </b></p>
                             <p><b>Available: </b></p>
                         </div>
                         <div class="imgDescription">
-                            <img src="Images/typeA.jpg" alt="Grey Hotel">
-                            <p><b>Room Type: </b></p>
-                            <p><b>Room Rate: </b></p>
+                            <img src="Images/mp_presidentialSuite.jpg" alt="Grey Hotel">
+                            <p><b>Room Type: </b>Presidential Suite</p>
+                            <p><b>Room Rate: </b>starts at Php 32,500/night</p>
                             <p><b>Room Number: </b></p>
                             <p><b>Available: </b></p>
                         </div>
                         </div>
                     </center>
-            <h3>Type C</h3> 
+            <h3>Continental Club</h3> 
                 <center>
                     <div class="imgRow">
                         <div class="imgDescription">
-                            <img src="Images/typeA.jpg" alt="Grey Hotel">
-                            <p><b>Room Type: </b></p>
-                            <p><b>Room Rate: </b></p>
+                            <img src="Images/mp_continentalClubBenefits.jpg" alt="Grey Hotel">
+                            <p><b>Room Type: </b>Continental Club Benefits</p>
+                            <p><b>Room Rate: </b>starts at Php 6,900/night</p>
                             <p><b>Room Number: </b></p>
                             <p><b>Available: </b></p>
                         </div>
                         <div class="imgDescription">
-                            <img src="Images/typeA.jpg" alt="Grey Hotel">
-                            <p><b>Room Type: </b></p>
-                            <p><b>Room Rate: </b></p>
+                            <img src="Images/mp_continentalDeluxeRoom.jpg" alt="Grey Hotel">
+                            <p><b>Room Type: </b>Continental Deluxe Room</p>
+                            <p><b>Room Rate: </b>starts at Php 7,300/night</p>
                             <p><b>Room Number: </b></p>
                             <p><b>Available: </b></p>
                         </div>
                         <div class="imgDescription">
-                            <img src="Images/typeA.jpg" alt="Grey Hotel">
-                            <p><b>Room Type: </b></p>
-                            <p><b>Room Rate: </b></p>
+                            <img src="Images/mp_continentalSuperiorSuite.jpg" alt="Grey Hotel">
+                            <p><b>Room Type: </b>Continental Superior Room</p>
+                            <p><b>Room Rate: </b>starts at Php 11,600/night</p>
+                            <p><b>Room Number: </b></p>
+                            <p><b>Available: </b></p>
+                        </div>
+                    </div>
+                </center>
+            <h3>Function Halls</h3> 
+                <center>
+                    <div class="imgRow">
+                        <div class="imgDescription">
+                            <img src="Images/mp_ballroom.jpg" alt="Grey Hotel">
+                            <p><b>Room Type: </b>Grey Ballroom</p>
+                            <p><b>Room Rate: </b>starts at Php 5,900</p>
+                            <p><b>Room Number: </b></p>
+                            <p><b>Available: </b></p>
+                        </div>
+                        <div class="imgDescription">
+                            <img src="Images/mp_functionHall.jpg" alt="Grey Hotel">
+                            <p><b>Room Type: </b>Grey Function Hall</p>
+                            <p><b>Room Rate: </b>starts at Php 5,000</p>
                             <p><b>Room Number: </b></p>
                             <p><b>Available: </b></p>
                         </div>
                     </div>
                 </center>
         </div>
+
+        <div class="Footer">
+        <footer class="footer-distributed">
+            <div class="footer-right">
+                <a href="#"><i class="fa fa-facebook"></i></a>
+                <a href="#"><i class="fa fa-twitter"></i></a>
+                <a href="#"><i class="fa fa-linkedin"></i></a>
+                <a href="#"><i class="fa fa-github"></i></a>
+            </div>
+            <div class="footer-left">
+                <p class="footer-links">
+                    <a href="index.php" uk-scroll>Home</a>.
+                    <a href="#">Pricing</a> .
+                    <a href="#about-hotel">About</a> .
+                    <a href="#">Contact</a> .
+                </p>
+                <p>Grey Hotel &copy; 2015</p>
+            </div>
+
+        </footer>
+    </div>
         
     </div>
     <script src="UIKIT/js/uikit.min.js"></script>
